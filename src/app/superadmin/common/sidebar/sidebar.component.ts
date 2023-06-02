9f449229fb80955cf723e20817c8164e7819e3bb
TR CodeTheWeb <codetheweb.bbsr@gmail.com>
2023-06-02 22:04:25 +0530

    Added multi-level expandabe menus to the sidebar.

--- src/app/superadmin/common/sidebar/sidebar.component.ts
+++ src/app/superadmin/common/sidebar/sidebar.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['../../../common.css', './sidebar.component.css']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["../../../common.css", "./sidebar.component.css"],
 })
 export class SidebarComponent {
-
+  wcList: any = [
+    {
+      name: "WC 1",
+      mccList: [
+        { name: "MCC 1" },
+        { name: "MCC 2" },
+        { name: "MCC 3" },
+        { name: "MCC 4" },
+        { name: "MCC 5" },
+      ],
+    },
+    {
+      name: "WC 2",
+      mccList: [
+        { name: "MCC 1" },
+        { name: "MCC 2" },
+        { name: "MCC 3" },
+        { name: "MCC 4" },
+        { name: "MCC 5" },
+      ],
+    },
+    {
+      name: "WC 3",
+      mccList: [
+        { name: "MCC 1" },
+        { name: "MCC 2" },
+        { name: "MCC 3" },
+        { name: "MCC 4" },
+        { name: "MCC 5" },
+      ],
+    },
+    {
+      name: "WC 4",
+      mccList: [
+        { name: "MCC 1" },
+        { name: "MCC 2" },
+        { name: "MCC 3" },
+        { name: "MCC 4" },
+        { name: "MCC 5" },
+      ],
+    },
+    {
+      name: "WC 5",
+      mccList: [
+        { name: "MCC 1" },
+        { name: "MCC 2" },
+        { name: "MCC 3" },
+        { name: "MCC 4" },
+        { name: "MCC 5" },
+      ],
+    },
+  ];
 }
