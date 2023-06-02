8a7e0b864847a619a644ea091e299eaebe15f28b
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2023-06-02 19:10:46 +0530

    @commit

--- src/app/superadmin/mcc/pit-view/script/script.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function(){ 
-var myModalEl = document.getElementById('pitenzymeModal')
-    myModalEl.addEventListener('hidden.bs.modal', function (event) {
-        document.getElementById("myForm").reset();
-})
- }, 100);
