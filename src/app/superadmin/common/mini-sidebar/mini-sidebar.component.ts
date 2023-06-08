f2efc697509149de6fe4063f3d8ce47225b79211
TR CodeTheWeb <codetheweb.bbsr@gmail.com>
2023-06-08 23:12:22 +0530

    Added the design for collapsible sidebar.

---
+++ src/app/superadmin/common/mini-sidebar/mini-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-sidebar',
+  templateUrl: './mini-sidebar.component.html',
+  styleUrls: ['./mini-sidebar.component.css']
+})
+export class MiniSidebarComponent {
+
+}
