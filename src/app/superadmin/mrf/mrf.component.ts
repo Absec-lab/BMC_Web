ee1634318e9f0e2a9886d8a491fce06ef14695d3
Shailendra <shailendra.biswal@gmail.com>
2023-06-10 20:38:27 +0530

    resolve subgoods issue

--- src/app/superadmin/mrf/mrf.component.ts
+++ src/app/superadmin/mrf/mrf.component.ts
@@ -30,7 +30,7 @@ export class MrfComponent implements OnInit{
   constructor(private service:CommonService, private formBuilder:FormBuilder){
     this.getList()
     this.getAllGoods()
-     this.getAllSubGoods() 
+     //this.getAllSubGoods() 
   }
   ngOnInit() {
     this.service.getAllMrf().subscribe(
