936c45ea72653b9548adf51bbdc87cfff662982e
Absec-lab <abseclabpvtltd@gmail.com>
2023-06-25 16:53:36 +0530

    @commit

--- src/app/service/common.service.ts
+++ src/app/service/common.service.ts
@@ -10,15 +10,15 @@ export class DeactivationDto {
 const environment = {
 
 
-//        URL: `http://15.207.62.200:9091`,  //prod url
-//        LOGIN_SERVICE_URL: 'http://15.207.62.200:8064/bmcwastemanagement/auth/users/login',
+       URL: `http://15.207.62.200:9091`,  //prod url
+       LOGIN_SERVICE_URL: 'http://15.207.62.200:8064/bmcwastemanagement/auth/users/login',
 
 
         //  LOGIN_SERVICE_URL: 'http://43.204.240.44:8064/bmcwastemanagement/auth/users/login',
         //  URL: `http://43.204.240.44:9091`  //Absec ip 
 
-        URL: `http://192.168.29.215:9091`,  //prod url
-       LOGIN_SERVICE_URL: 'http://15.207.62.200:8064/bmcwastemanagement/auth/users/login',
+//         URL: `http://192.168.29.215:9091`,  //prod url
+//        LOGIN_SERVICE_URL: 'http://15.207.62.200:8064/bmcwastemanagement/auth/users/login',
 
 
         // LOGIN_SERVICE_URL: 'http://15.207.62.200:8064/bmcwastemanagement/auth/users/login',
