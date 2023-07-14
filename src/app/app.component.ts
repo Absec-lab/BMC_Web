4603d83321ef20fd288ca473c43ecc03b9961adb
Tara Prasad Routray <codetheweb.bbsr@gmail.com>
2023-07-14 21:34:08 +0530

    Corrected some sidebar issues.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent {
               const sidebarParentElement = document.querySelector('.sidebar-parent') as HTMLDivElement;
               sidebarParentElement && sidebarParentElement.classList.toggle('d-none');
               const appContentElement = document.querySelector('.app-content') as HTMLDivElement;
-              appContentElement && appContentElement.classList.toggle('col-lg-9');
+              appContentElement && appContentElement.classList.toggle('col-xl-9');
               appContentElement && appContentElement.classList.toggle('col-12');
             }, 10);
           }
